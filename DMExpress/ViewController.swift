23c3601b5a9d7d9a6b75a7ffe34da353ecd93f2e
Narlei Américo Moreira <narlei.guitar@gmail.com>
2019-02-08 13:15:06 -0200

    Change correct version]

--- DMExpress/ViewController.swift
+++ DMExpress/ViewController.swift
@@ -6,15 +6,56 @@
 //  Copyright © 2019 Narlei A Moreira. All rights reserved.
 //
 
+import BringgTracking
 import UIKit
 
+let token = ""
+
 class ViewController: UIViewController {
+    var trackClient: BringgTrackingClient!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        trackClient = BringgTrackingClient(developerToken: token, connectionDelegate: self)
+        
+        trackClient.connect()
+        
+        
+
     }
-
-
 }
 
+extension ViewController: RealTimeDelegate {
+    func trackerDidConnect() {
+        print("Connected")
+        trackClient.startWatchingOrder(withUUID: "18727c2b-b382-4a21-a0b8-e1834210d6ab", customerAccessToken: token, delegate: self)
+//        trackClient.startWatchingOrder(withShareUUID: "18727c2b-b382-4a21-a0b8-e1834210d6ab", delegate: self)
+
+    }
+
+    func trackerDidDisconnectWithError(_ error: Error?) {
+        print(error)
+    }
+}
+
+extension ViewController: OrderDelegate {
+    func watchOrderSucceed(for order: GGOrder) {
+        print(order)
+    }
+
+    func watchOrderFail(for order: GGOrder, error: Error) {
+        print(error)
+    }
+
+    func orderDidAssign(with order: GGOrder, with driver: GGDriver) {
+        print(driver)
+    }
+
+    func orderDidAccept(with order: GGOrder, with driver: GGDriver) {
+        print(driver)
+    }
+
+    func orderDidStart(with order: GGOrder, with driver: GGDriver) {
+        print(driver)
+    }
+}
